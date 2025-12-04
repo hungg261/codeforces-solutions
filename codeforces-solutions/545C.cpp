cb9df2cb60e9d76c207a58ed1bbb816890ba9e86
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-04 19:30:27 +0700

    new

--- codeforces-solutions/545C.cpp
+++ codeforces-solutions/545C.cpp
@@ -10,12 +10,16 @@ struct Tree{
         return this->x < other.x;
     }
 
-    bool collapse(const Tree& other){
-        return this->x + this->h < other.x - other.h;
+    bool together(const Tree& other){
+        return other.x + other.h < x - h;
     }
 
-    bool contain(const Tree& other){
-        return this->x <= other.x && other.x <= this->x + this->h;
+    bool touchL(const Tree& other){
+        return x - h <= other.x && other.x <= x;
+    }
+
+    bool touchR(const Tree& other){
+        return x <= other.x && other.x <= x + h;
     }
 
     void input(){
@@ -24,61 +28,27 @@ struct Tree{
 };
 
 const int MAXN = 1e5;
-int dp[MAXN + 5][3], maxDp[MAXN + 5][3], n;
 Tree T[MAXN + 5];
+int n;
 
-int bound_less(int target){
-    int l = 1, r = n, res = 0;
-    while(l <= r){
-        int mid = (l + r) >> 1;
-        if(T[mid].x <= target){
-            res = mid;
-            l = mid + 1;
-        }
-        else r = mid - 1;
-    }
-
-    return res;
-}
-
+bool fallR[MAXN + 5];
 void solve(){
-    dp[1][0] = 1;
-    dp[1][1] = 0;
-    if(n >= 2 && T[1].collapse(T[2])) dp[1][2] = 1;
+    int last = -1e18;
+    int ans = 1;
 
-    for(int i = 2; i <= n; ++i){
-        if(T[i - 1].collapse(T[i])){
-            dp[i][0] = max({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]}) + 1;
-            dp[i][1] = max({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]});
-            dp[i][2] = max({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]});
-            if(i < n && T[i].collapse(T[i + 1])) ++dp[i][2];
+    for(int i = 1; i <= n - 1; ++i){
+        if(T[i].x - T[i].h > last){
+            last = T[i].x;
+            ++ans;
         }
-        else if(T[i - 1].contain(T[i])){
-            dp[i][0];
+        else if(T[i].x + T[i].h < T[i + 1].x){
+            last = T[i].x + T[i].h;
+            ++ans;
         }
-        else{
-            dp[i][0] = max({dp[i - 1][0], dp[i - 1][1]}) + 1;
-            dp[i][1] = max({dp[i - 1][0], dp[i - 1][1]})
-        }
+        else last = T[i].x;
     }
 
-//    dp[1][0] = 1;
-//    for(int i = 2; i <= n; ++i){
-//        if(T[i - 1].x + T[i - 1].h < T[i].x - T[i].h){
-//            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]) + 1;
-//
-//            dp[i][1] = max(dp[i - 1][0], dp[i - 1][1]);
-//            if(i < n && T[i].x + T[i].h < T[i + 1].x - T[i].h){
-//                dp[i][1]++;
-//            }
-//
-////            int idx = bound_less(T[i].x - T[i].h - 1);
-////            dp[i][0] = max(maxDp[idx][0], maxDp[idx][1]) + 1;
-//        }
-//        else{
-//            dp[i][0] = max(dp[i - 1][1], dp[i - 1][0] + 1);
-//        }
-//    }
+    cout << ans << '\n';
 }
 
 signed main(){
