ec42d7511dcaddf369c0ce3d3e7fa09819316443
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-19 13:56:56 +0700

    new

---
+++ codeforces-solutions/2104E.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e6;
+int n, k, q;
+string s;
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> k;
+    cin >> s;
+    cin >> q;
+
+
+    return 0;
+}
