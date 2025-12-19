3617c4fac5f8320f7c75ed1e822d7b1099c01851
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-19 18:45:23 +0700

    new

---
+++ codeforces-solutions/2005C.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+//#define int long long
+using namespace std;
+
+void solve(){
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
