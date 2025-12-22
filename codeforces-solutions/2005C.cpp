233fc5c2b1367cd796667cb0116282fb22f11e04
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-22 18:49:45 +0700

    new

--- codeforces-solutions/2005C.cpp
+++ codeforces-solutions/2005C.cpp
@@ -2,10 +2,8 @@
 //#define int long long
 using namespace std;
 
-const string TARGET = "narek";
-int encode(char c){
-    return find(begin(TARGET), end(TARGET), c) - begin(TARGET);
-}
+const string BASE = "narek";
+int SZ = BASE.size();
 
 void solve(){
     int n, m;
@@ -13,37 +11,47 @@ void solve(){
 
     vector<string> arr(n + 1);
     for(int i = 1; i <= n; ++i){
-        cin >> arr[i];
-        arr[i] = "#" + arr[i];
-    }
-
-    vector<vector<vector<int>>> nxt(n + 1, vector<vector<int>>(m + 1, vector<int>(5, 0)));
-    int last[6] = {};
-    for(int i = 1; i <= n; ++i){
-        for(int j = n; j >= 0; --j){
-            for(int c = 0; c < 5; ++c){
-                nxt[i][j][c] = last[c];
-            }
-
-            if(j > 0) last[encode(arr[i][j] - 'a')] = j;
-        }
-    }
-
-    vector<vector<int>> dp(n + 1, vector<int>(5, 0));
-    for(int i = 1; i <= n; ++i){
+        string line; cin >> line;
         for(int j = 0; j < m; ++j){
-            for(int prv_c = 0; prv_c < 5; ++prv_c){
-
-            }
+            if(find(BASE.begin(), BASE.end(), line[j]) != BASE.end())
+                arr[i] += line[j];
         }
     }
+
+    vector<vector<int>> dp(n + 1, vector<int>(SZ, -1000000000));
+    dp[0][SZ - 1] = 0;
+
+    for(int i = 1; i <= n; ++i){
+        for(int c = 0; c < SZ; ++c) dp[i][c] = dp[i - 1][c];
+        for(int c = 0; c < SZ; ++c){
+            int target = (c + 1) % SZ;
+            int cur = 0;
+
+            int ssz = arr[i].size();
+            for(int j = 0; j < ssz; ++j){
+                --cur;
+
+                if(arr[i][j] == BASE[target]){
+                    if(target == SZ - 1){
+                        cur += 10;
+                    }
+                    target = (target + 1) % SZ;
+                }
+            }
+
+            target = (target - 1 + SZ) % SZ;
+            dp[i][target] = max(dp[i][target], dp[i - 1][c] + cur);
+        }
+    }
+
+    cout << *max_element(begin(dp[n]), end(dp[n])) << '\n';
 }
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     int t = 1;
-    //cin >> t;
+    cin >> t;
     while(t--){
         solve();
     }
