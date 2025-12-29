da32197589c7d345a2e73c194afbdca73cc11af0
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-29 14:01:06 +0700

    new

--- codeforces-solutions/900C.cpp
+++ codeforces-solutions/900C.cpp
@@ -1,60 +1,74 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct FenwickTree{
+    int n;
+    vector<int> BIT;
+
+    FenwickTree() = default;
+    FenwickTree(int sz){ n = sz; BIT.resize(sz + 1, 0); }
+
+    void update(int idx, int val){
+        if(idx <= 0) return;
+
+        for(int i = idx; i <= n; i += i & -i){
+            BIT[i] += val;
+        }
+    }
+
+    int get(int idx){
+        if(idx <= 0) return 0;
+
+        int res = 0;
+        for(int i = idx; i > 0; i -= i & -i){
+            res += BIT[i];
+        }
+        return res;
+    }
+
+    int get(int l, int r){ if(l > r) return 0; return get(r) - get(l - 1); }
+};
+
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     int n;
     cin >> n;
 
-    vector<int> arr(n + 1), delta(n + 1, 0), preRec(n + 1, 0), sufRec(n + 2, 0);
-
-    arr[0] = 0;
-    preRec[0] = 0, sufRec[0] = 0;
-    pair<int, int> maxVal = {0, 0};
+    vector<int> arr(n + 1);
     for(int i = 1; i <= n; ++i){
         cin >> arr[i];
-
-        preRec[i] = preRec[i - 1];
-        if(arr[i] > arr[maxVal.first]) ++delta[maxVal.first];
-        if(arr[i] > arr[maxVal.first]) ++sufRec[i];
-
-        if(arr[i] > arr[maxVal.first]){
-            maxVal.second = maxVal.first;
-            maxVal.first = i;
-        }
-        else if(arr[i] > arr[maxVal.second]){
-            maxVal.second = i;
-        }
-
-        if(arr[maxVal.second] > arr[i]) continue;
-        else if(arr[maxVal.first] > arr[i]){
-            ++delta[maxVal.first];
-        }
     }
 
-    if(n == 2){
-        cout << "1\n";
-        return 0;
-    }
-
-    for(int i = n; i >= 1; --i){
-        sufRec[i] = sufRec[i + 1] + sufRec[i];
-    }
-
-    int resRec = 0, res = 1e9;
+    FenwickTree fwt(n);
+    vector<int> already(n + 1, 0);
+    vector<int> preLarger(n + 1, 0);
     for(int i = 1; i <= n; ++i){
-        int newRec = preRec[i - 1] + sufRec[i + 1] + delta[i];
-        if(newRec > resRec){
-            res = arr[i];
-            resRec = newRec;
-        }
-        else if(newRec == resRec) res = min(res, arr[i]);
+        preLarger[i] = fwt.get(arr[i], n);
 
-        cerr << i << " " << arr[i] << ": " << preRec[i - 1] << ' ' << delta[i] << '\n';
+        already[i] = already[i - 1] + (preLarger[i] == 0);
+        fwt.update(arr[i], 1);
     }
 
-    cout << res << '\n';
+    FenwickTree cnt1(n);
+
+    int resCnt = -1, resVal = -1;
+    for(int i = n; i >= 1; --i){
+        int candidate = already[n] - (already[i] - already[i - 1]) + cnt1.get(arr[i]);
+        if(candidate > resCnt){
+            resVal = arr[i];
+            resCnt = candidate;
+        }
+        else if(candidate == resCnt){
+            resVal = min(resVal, arr[i]);
+        }
+
+        if(preLarger[i] == 1){
+            cnt1.update(arr[i], 1);
+        }
+    }
+
+    cout << resVal << '\n';
 
     return 0;
 }
