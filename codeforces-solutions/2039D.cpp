4ca2f9375a66480f8c4145617e6a22e5ae7a3538
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-31 22:12:43 +0700

    start

---
+++ codeforces-solutions/2039D.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+Link: https://codeforces.com/contest/2039/problem/D
+Code: 2039D
+Time (YYYY-MM-DD-hh.mm.ss): 2025-12-31-22.12.01
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+ 
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+
+    return 0;
+}
