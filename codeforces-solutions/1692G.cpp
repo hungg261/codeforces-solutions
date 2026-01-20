59927ee31ad9c6ac4cf6dcfcf5ead6e79805b9ae
hungg261 <phanthanhhung.26012010@gmail.com>
2026-01-20 21:27:14 +0700

    new

--- codeforces-solutions/1692G.cpp
+++ codeforces-solutions/1692G.cpp
@@ -1,33 +1,29 @@
 /******************************************************************************
 Link: https://codeforces.com/problemset/problem/1692/G
 Code: 1692G
-Time (YYYY-MM-DD-hh.mm.ss): 2026-01-13-12.58.52
+Time (YYYY-MM-DD-hh.mm.ss): 2026-01-20-21.21.46
 *******************************************************************************/
 #include<bits/stdc++.h>
-//#define int long long
 using namespace std;
 
 void solve(){
     int n, k;
     cin >> n >> k;
 
-    vector<int> arr(n + 1);
+    vector<int> arr(n + 1), pre(n + 1);
+    pre[0] = 0;
     for(int i = 1; i <= n; ++i){
         cin >> arr[i];
+
+        if(i > 1 && arr[i - 1] < 2 * arr[i]){
+            ++pre[i - 1];
+        }
+        pre[i] = pre[i - 1];
     }
 
     int res = 0;
-    for(int i = 1; i < n; ++i){
-        for(int j = i + 1; j <= n; ++j){
-            if(arr[j - 1] >= arr[j] * 2){
-                break;
-            }
-
-            if(j - i + 1 == k + 1){
-                ++res;
-                break;
-            }
-        }
+    for(int i = 1; i <= n - k; ++i){
+        if(pre[i + k - 1] - pre[i - 1] == k) ++res;
     }
 
     cout << res << '\n';
