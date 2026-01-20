5940cc0951507e1d3e52044f3c5256413013a44d
hungg261 <phanthanhhung.26012010@gmail.com>
2026-01-20 16:12:36 +0700

    new

---
+++ codeforces-solutions/982E.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+Link: https://codeforces.com/contest/982/problem/E
+Code: 982E
+Time (YYYY-MM-DD-hh.mm.ss): 2026-01-20-16.12.20
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+
+    return 0;
+}
