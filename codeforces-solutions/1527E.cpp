f69b5ecbd0316cf27a02b6bacff30a8823cc6deb
hungg261 <phanthanhhung.26012010@gmail.com>
2026-02-07 21:57:04 +0700

    Auto commit

--- codeforces-solutions/1527E.cpp
+++ codeforces-solutions/1527E.cpp
@@ -6,13 +6,28 @@ Time (YYYY-MM-DD-hh.mm.ss): 2026-02-01-00.13.14
 #include<bits/stdc++.h>
 using namespace std;
 
+#define int long long
+
 const int MAXN = 35000, MAXK = 100;
 int arr[MAXN + 5], n, k;
 int dp[MAXK + 5][MAXN + 5];
 
-vector<vector<int>> pos[MAXN + 5];
-
 // test dp / expected TLE
+int w(int l, int r){
+    map<int, pair<int, int>> mp;
+    for(int i = l; i <= r; ++i){
+        if(mp.count(arr[i]))
+            mp[arr[i]].second = i;
+        else mp[arr[i]] = {i, i};
+    }
+
+    int res = 0;
+    for(const pair<int, pair<int, int>>& p: mp){
+        res += p.second.second - p.second.first;
+    }
+    return res;
+}
+
 void trau(){
     memset(dp, 0x3f, sizeof dp);
     dp[0][0] = 0;
@@ -20,7 +35,7 @@ void trau(){
     for(int c = 1; c <= k; ++c){
         for(int i = 1; i <= n; ++i){
             for(int j = 1; j <= i; ++j){
-                dp[c][i] = min(dp[c][i], dp[c - 1][j - 1] + cost(j, i));
+                dp[c][i] = min(dp[c][i], dp[c - 1][j - 1] + w(j, i));
             }
         }
     }
@@ -28,22 +43,60 @@ void trau(){
     cout << dp[k][n] << '\n';
 }
 
+int pre[MAXN + 5], suf[MAXN + 5];
+
 int curCost = 0;
 int L, R;
 
-void add(int x){
-    int lo = lower_bound(pos[x].begin(), pos[x].end(), L) - pos[x].begin();
-    int hi = upper_bound(pos[x].begin(), pos[x].end(), R) - pos[x].begin() - 1;
-    if(lo <= hi) curCost += hi - lo;
+void addL(int idx){
+    curCost += pre[idx];
+}
+
+void addR(int idx){
+    curCost += suf[idx];
+}
+
+void popL(int idx){
+    curCost -= pre[idx];
+}
+
+void popR(int idx){
+    curCost -= suf[idx];
 }
 
 void move(int l, int r){
-    while(L > l) add(arr[--L]);
-    while(R < r) add(arr[++R]);
+    while(L > l) addL(--L);
+    while(R < r) addR(++R);
+    while(L < l) popL(L++);
+    while(R > r) popR(R--);
 }
 
-void compute(){
+void computeDp(int c, int l, int r, int optL, int optR){
+    if(l > r) return;
 
+    int mid = (l + r) >> 1;
+    pair<int, int> best = {LLONG_MAX, -1};
+    for(int i = optL; i <= min(optR, mid); ++i){
+        move(i, mid);
+        best = min(best, {dp[c - 1][i - 1] + curCost, i});
+    }
+
+    dp[c][mid] = best.first;
+    int opt = best.second;
+
+    computeDp(c, l, mid - 1, optL, opt);
+    computeDp(c, mid + 1, r, opt, optR);
+}
+
+void solve(){
+    memset(dp, 0x3f, sizeof dp);
+    dp[0][0] = 0;
+
+    for(int c = 1; c <= k; ++c){
+        L = 1; R = 0;
+        computeDp(c, 1, n, 1, n);
+    }
+    cout << dp[k][n] << '\n';
 }
 
 signed main(){
@@ -51,8 +104,30 @@ signed main(){
     cin >> n >> k;
     for(int i = 1; i <= n; ++i){
         cin >> arr[i];
-        pos[arr[i]].push_back(i);
     }
+
+    vector<int> last(n + 1);
+    fill(begin(last) + 1, end(last), -1);
+
+    for(int i = 1; i <= n; ++i){
+        pre[i] = pre[i - 1];
+        if(last[arr[i]] != -1){
+            pre[i] = i - last[arr[i]];
+        }
+
+        last[arr[i]] = i;
+    }
+
+    fill(begin(last) + 1, end(last), -1);
+    for(int i = n; i >= 1; --i){
+        if(last[arr[i]] != -1){
+            suf[i] = last[arr[i]] - i;
+        }
+
+        last[arr[i]] = i;
+    }
+
+    solve();
 
     return 0;
 }
