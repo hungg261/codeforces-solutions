680e3aff2d821a2dccbba95c0a554f9ffd61c73c
hungg261 <phanthanhhung.26012010@gmail.com>
2026-02-10 16:44:00 +0700

    Auto commit

--- codeforces-solutions/1856C.cpp
+++ codeforces-solutions/1856C.cpp
@@ -1,44 +1,55 @@
 /******************************************************************************
 Link: https://codeforces.com/contest/1856/problem/C
 Code: 1856C
-Time (YYYY-MM-DD-hh.mm.ss): 2026-02-09-18.03.40
+Time (YYYY-MM-DD-hh.mm.ss): 2026-02-10-11.50.52
 *******************************************************************************/
 #include<bits/stdc++.h>
 using namespace std;
+
+#define int long long
+
+const int INF = 1e15;
+
+int f(int i, int h, const vector<int>& arr){
+    if(i >= (int)arr.size()) return INF;
+    if(h <= arr[i]) return 0;
+    return h - arr[i] + f(i + 1, h - 1, arr);
+}
+
+bool check(int mid, int k, const vector<int>& arr){
+    for(int i = 0; i < (int)arr.size(); ++i){
+        if(f(i, mid, arr) <= k){
+            return true;
+        }
+    }
+    return false;
+}
 
 void solve(){
     int n, k;
     cin >> n >> k;
 
-    vector<int> arr(n + 1);
-    for(int i = 1; i <= n; ++i){
-        cin >> arr[i];
+    vector<int> arr(n);
+    for(int& ele: arr) cin >> ele;
+
+    int l = 0, r = 1e9, res = -1;
+    while(l <= r){
+        int mid = (l + r) >> 1;
+        if(check(mid, k, arr)){
+            res = mid;
+            l = mid + 1;
+        }
+        else r = mid - 1;
     }
 
-    while(k > 0){
-        int idx = -1;
-        for(int i = 2; i <= n; ++i){
-            if(arr[i - 1] <= arr[i] && (idx == -1 || arr[idx] < arr[i]))
-                idx = i;
-        }
-
-        if(idx == -1) break;
-        for(int i = idx - 1; i >= 1 && k > 0; --i){
-            int cost = min(k, arr[i + 1] - arr[i] + 1);
-
-            k -= cost;
-            arr[i] += cost;
-        }
-    }
-
-    for(int ele: arr) cout << ele << ' '; cout << '\n';
+    cout << res << '\n';
 }
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     int t = 1;
-    //cin >> t;
+    cin >> t;
 
     while(t--){
         solve();
