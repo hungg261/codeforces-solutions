7dba7071d723c78e74dcbe900af2585c832cd977
hungg261 <phanthanhhung.26012010@gmail.com>
2026-02-13 12:53:37 +0700

    Auto commit

---
+++ codeforces-solutions/2184D.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+Link: https://codeforces.com/contest/2184/problem/D
+Code: 2184D
+Time (YYYY-MM-DD-hh.mm.ss): 2026-02-12-16.12.51
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+
+    int LG = __lg(n);
+    for(int len = 1; len < LG; ++len){
+
+    }
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
