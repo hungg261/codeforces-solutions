6a5db9fd478c791493fb14a8f93e13905acc52fe
hungg261 <phanthanhhung.26012010@gmail.com>
2026-02-23 19:16:55 +0700

    Auto group commit (id=2302-1916)

---
+++ codeforces-solutions/1628E.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+Link: https://codeforces.com/contest/1628/problem/E
+Code: 1628E
+Time (YYYY-MM-DD-hh.mm.ss): 2026-02-23-10.53.24
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 3e5;
+int n, q;
+vector<int> adj[MAXN + 5];
+int tin[MAXN + 5];
+
+void solve(){
+    for(int i = 1; i <= n; ++i){
+
+    }
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
